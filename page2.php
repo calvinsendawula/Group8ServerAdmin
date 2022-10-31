54ba617426e094f42b1d25834c8cdefc7c09cbaf
calvinsendawula <calvin.sendawula@strathmore.edu>
2022-10-31 19:25:07 +0300

    Initialize

---
+++ page2.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fontawesome -->
+    <script src="https://kit.fontawesome.com/d30f67c531.js" crossorigin="anonymous"></script>
+
+    <!-- Google Icons -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <style>
+        html, body{
+            color: #000;
+            background-color: rgb(235, 225, 225);
+        }
+    </style>
+    <title>Group 8 Server Admin</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-8 my-3">
+                <div class="row">
+                    <div class="col my-3">
+                        <h2>Group 8 Server Admin Members ICS 3.2A</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>Student No.</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Name</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>129277</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Bosibori Cindy Sydney</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>135012</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Wasike Jean Gloria</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>136047</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Sendawula Calvin Jonathan</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>131590</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Kendi Njeru</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>134022</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Mwikali Assumpta</h3>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-3 my-3">
+                        <h3>136809</h3>
+                    </div>
+                    <div class="col-9 my-3">
+                        <h3>Fleskine Omondi</h3>
+                    </div>
+                </div>
+                <a class="btn btn-outline-danger" href="index.php">Invert Color</a>
+            </div>
+        </div>
+    </div>   
+</body>
+</html>
